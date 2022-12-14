a8917a0ab6a1406e324a957b2494fae421eda17b
Artem Bulkhak <artem.bulkhak@mail.ru>
2022-12-14 11:24:43 +0300

    Add the whole critical path visualization
    - Add a red highlite to the critical path through Graphviz
    - Refactor

--- DAG.py
+++ DAG.py
@@ -1,36 +1,12 @@
 from multipledispatch import dispatch
 
+from Visualization import GraphvizModule
+
 """
 TODO:
 	1) Ввести таблицу производительности процессоров для каждой из подзадач
-	2) Добавить в критический путь вермя выполнения задания 
-	3) Сделать выдачу упорядоченного (по априорному времени выполнения) 
-	массива всех критических путей
-	4) Объединить с модулем визуализации
-	5) Раскрашивать критический путь на графе (посмотреть вохможности Graphviz,
-	 если нет то NetworkX из гитхаба)
+	2) Сравнить возможности Graphviz и NetworkX (из github)
 """
-
-class Node:  # Job
-
-    def __init__(self, id, name, runtime):
-        self.id = id
-        self.name = name
-        self.runtime = runtime
-        self.visited = False
-        self.children = []
-        self.cp = [id]
-        self.cp_length = -1
-
-
-    def add_children(self, node):
-        self.children.append(node)
-
-
-    def __str__(self):
-        return 'id = ' + str(self.id) + \
-               ", name = " + self.name + \
-               ", runtime = " + str(self.runtime)
 
 class DAG:  # JobFlow
 
@@ -41,45 +17,68 @@ class DAG:  # JobFlow
         self.start_edges = None
         self.finish_edges = None
         self.dp = []        # dynamic programming table
-        self.add_node(0, 'entry', 0.0)
+        self.graphviz_nodes = []
+        self.graphviz_edges = []
 
 
     def set_dp(self):
-        # n = len(self.nodes)
-        n = len(self.node_dict)
-        self.dp = [[0] * (n+1) for i in range(n+1)] # n+1 = + finish_node
-        self.start_edges = [0 for i in range(n+1)]
-        self.finish_edges = [0 for i in range(n+1)]
+        n = len(self.nodes)
+        self.dp = [[0] * (n) for i in range(n)]
+        self.start_edges = [0 for i in range(n)]
+        self.finish_edges = [0 for i in range(n)]
 
-    @dispatch(int, str, float)
-    def add_node(self, id, name, runtime):
-        node = Node(id, name, runtime)
-        self.nodes.append(node)
-        self.node_dict[node.id] = node
 
-    @dispatch(Node)
     def add_node(self, node):
         self.nodes.append(node)
-        self.node_dict[node.id] = node
+        self.graphviz_nodes.append(node)
+        self.node_dict[node.name] = node
 
 
     def add_edge(self, edge):
         self.edges.append(edge)
+        self.graphviz_edges.append(edge)
         edge.source_node.add_children(edge.destination_node)
         self.dp[edge.source_node.id][edge.destination_node.id] = edge.transfer_time
 
         self.start_edges[edge.destination_node.id] = 1
         self.finish_edges[edge.source_node.id] = 1
 
+    def design_graph(self, soup_nodes, soup_edges):
+
+        # Add entry_node into graph
+        self.add_node(Node(0, 'entry', 0.0))
+
+        i = 1
+        for node in soup_nodes:
+            name = node.get('id')
+            runtime = node.get('runtime')
+            self.add_node(Node(i, name, runtime))
+            i += 1
+
+        # Add finish_node into graph
+        self.add_node(Node(len(self.nodes), 'finish', 0.0))
+        self.set_dp()
+
+        for edge in soup_edges:
+            parents = edge.find_all('parent')
+            for parent in parents:
+                node_from = self.node_dict.get(parent.get('ref'))
+                node_to = self.node_dict.get(edge.get('ref'))
+                transfer_time = float(parent.get('transfertime'))
+                self.add_edge(Edge(node_from, node_to, transfer_time))
+
+
+        print(self.findLongestPath())
+
+        GraphvizModule.graphviz_run(self.graphviz_nodes, self.graphviz_edges)
+
     def complete_graph(self):
         n = len(self.nodes)
-        for i in range(1, n):
+        for i in range(1, n-1):
             if self.start_edges[i] == 0:
                 self.add_edge(Edge(self.nodes[0], self.nodes[i], 0.0))
 
-        self.add_node(Node(len(self.nodes), 'finish', 0.0))
-
-        for i in range(1, n):
+        for i in range(1, n-1):
             if self.finish_edges[i] == 0:
                 self.add_edge(Edge(self.nodes[i], self.nodes[-1], 0.0))
 
@@ -92,6 +91,9 @@ class DAG:  # JobFlow
 
         for node in self.nodes:
             print(node.id, node.cp_length, node.cp)
+
+        for i in self.nodes[0].cp:
+            self.nodes[i].color = 'red'
 
         return self.nodes[0].cp_length, self.nodes[0].cp
 
@@ -116,7 +118,26 @@ class DAG:  # JobFlow
                     node.cp_length = dp[node.id][child.id]
 
 
+class Node:  # Job
+    def __init__(self, id, name, runtime):
+        self.id = id
+        self.name = name
+        self.runtime = runtime
+        self.visited = False
+        self.children = []
+        self.cp = [id]
+        self.cp_length = -1
+        self.color = 'black'
 
+
+    def add_children(self, node):
+        self.children.append(node)
+
+
+    def __str__(self):
+        return 'id = ' + str(self.id) + \
+               ", name = " + self.name + \
+               ", runtime = " + str(self.runtime)
 
 
 
@@ -132,30 +153,3 @@ class Edge:  # Data transfer
         return str(self.source_node.id) + \
                " --> " + str(self.destination_node.id) + \
                ", transfer_time = " + str(self.transfer_time)
-
-
-
-
-if __name__ == "__main__":
-
-    g = DAG()
-    g.add_node(0, 1)
-    g.add_node(1, 2)
-    g.add_node(2, 3)
-    g.add_node(3, 4)
-
-    g.set_dp()
-
-    g.add_edge(0, 1, 1)
-    g.add_edge(0, 2, 5)
-    g.add_edge(2, 1, 6)
-    g.add_edge(1, 3, 7)
-    g.add_edge(2, 3, 2)
-
-    for i in g.nodes:
-        print(i)
-
-    for i in g.edges:
-        print(i)
-
-    print(g.findLongestPath())
